9f4f67680268019c767ba7f07e2359ec3886cd0b
chen <chenyulei91@gmail.com>
2017-01-19 16:40:11 +0800

    Initial commit

---
+++ app/src/main/java/com/cicue/view/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cicue.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baiyyyhjl.listshowmore.R;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
